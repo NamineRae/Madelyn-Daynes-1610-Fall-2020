e60c3ed88ff291d5c03c653fae731e2d366c1e17
Madelyn Daynes <maddy.daynes@gmail.com>
2020-10-10 20:44:26 -0600

    Started working on Personal Project

---
+++ Fall 2020 Personal Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject Player;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        //get camera to follow player
+        transform.position = Player().transform.position;
+    }
+}
---
+++ Fall 2020 Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public float verticalInput;
+    public float horizontalInput;
+    void Update()
+    {
+        //get user's input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+    }
+}
