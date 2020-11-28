af8baaf799960cfd4df57c56b8df10338947440d
Madelyn Daynes <maddy.daynes@gmail.com>
2020-11-27 17:45:27 -0700

    Finished Prototype 4

--- Prorotype 4 Sumo Battle/Assets/Scripts/Enemy.cs
+++ Prorotype 4 Sumo Battle/Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -18,5 +19,10 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
