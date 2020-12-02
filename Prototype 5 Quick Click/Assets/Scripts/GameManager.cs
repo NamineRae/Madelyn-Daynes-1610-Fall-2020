631c4f077a099f124e7a0eeaef49fd0b2f9bfd71
Madelyn Daynes <maddy.daynes@gmail.com>
2020-12-02 02:36:13 -0700

    Startedm5.1

--- Prototype 5 Quick Click/Assets/Scripts/GameManager.cs
+++ Prototype 5 Quick Click/Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
         
     }
 
-    IEnumerator SpawnTarget()
+   /* IEnumerator SpawnTarget()
     {
         while (true)
         {
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count);
             Instantiate(targets<index>);
         }
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
--- Prototype 5 Quick Click/Assets/Scripts/Target.cs
+++ Prototype 5 Quick Click/Assets/Scripts/Target.cs
@@ -11,8 +11,9 @@ public class Target : MonoBehaviour
     {
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-10,10), Random.Range(-10, 10), Random.Range(-10, 10));
-        transform.position = new Vector3(Random.Range(-4, 4), -6, 0);
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
+        //Random.Range(-10, 10), ForceMode.Impulse);
     }
 
     // Update is called once per frame
