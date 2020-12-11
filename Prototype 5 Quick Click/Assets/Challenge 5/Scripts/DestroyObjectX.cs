d27491b53f3303205a5fbe4925d947e0c8b205b4
Madelyn Daynes <maddy.daynes@gmail.com>
2020-12-11 02:47:45 -0700

    Started Challenge 5

---
+++ Prototype 5 Quick Click/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject); // destroy particle when clicked on
+        //whoops this was the destroy after two seconds script
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
